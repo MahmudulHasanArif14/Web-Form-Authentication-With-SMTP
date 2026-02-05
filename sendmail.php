f7f8e6fb33a353f313c6346220905928316dadf9
Mahmudul Hasan Arif <68496610+MahmudulHasanArif14@users.noreply.github.com>
2026-02-05 23:13:10 +0600

    Use environment variables for SMTP settings in sendMail
    Refactor sendMail function to use environment variables for SMTP configuration.

--- sendmail.php
+++ sendmail.php
@@ -7,14 +7,32 @@ function sendMail($recipient, $subject, $body) {
     $mail = new PHPMailer(true);
     try {
         $mail->isSMTP();
-        $mail->Host       = 'smtp.gmail.com';
-        $mail->SMTPAuth   = true;
-        $mail->Username   = 'cse_0182220012101050@lus.ac.bd';
-        $mail->Password   = '';
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-        $mail->Port       = 587;
 
-        $mail->setFrom('cse_0182220012101050@lus.ac.bd', 'Md. Mahmudul Hasan Arif');
+        $mail->Host     = $_ENV['SMTP_HOST'];
+        $mail->Username = $_ENV['SMTP_USER'];
+        $mail->Password = $_ENV['SMTP_PASS'];
+        $mail->Port     = $_ENV['SMTP_PORT'];
+        $mail->SMTPAuth   = true;
+
+
+
+        
+
+        
+        // $mail->Host       = 'smtp.gmail.com';
+        // $mail->Username   = 'cse_0182220012101050@lus.ac.bd';
+        // $mail->Password   = '';
+        
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        // $mail->Port       = 587;
+
+
+        $mail->setFrom($_ENV['SMTP_USER'], 'Md. Mahmudul Hasan Arif');
+        $mail->addReplyTo('cse_0182220012101050@lus.ac.bd', 'Md. Mahmudul Hasan Arif');
+
+
+        
+        // $mail->setFrom('cse_0182220012101050@lus.ac.bd', 'Md. Mahmudul Hasan Arif');
         $mail->addAddress($recipient);
 
         $mail->isHTML(true);

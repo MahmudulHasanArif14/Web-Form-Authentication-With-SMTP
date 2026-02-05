3d8fa55e7dd2cf7706bdf213aec366dd11c48afb
Mahmudul Hasan Arif <68496610+MahmudulHasanArif14@users.noreply.github.com>
2026-02-05 23:01:13 +0600

    Update database connection method
    Refactored database connection to use environment variables.

--- connection.php
+++ connection.php
@@ -1,9 +1,21 @@
 <?php
-$server="localhost";
-$usrnm="root";
-$pass="";
-$dbname="formvalidation";
-$con=mysqli_connect($server,$usrnm,$pass,$dbname);
+
+$host = $_ENV['DB_HOST'];
+$db   = $_ENV['DB_NAME'];
+$user = $_ENV['DB_USERNAME'];
+$pass = $_ENV['DB_PASSWORD'];
+$port = $_ENV['DB_PORT'];
+
+$con = new mysqli($host, $user, $pass, $db, $port);
+
+
+
+
+// $server="localhost";
+// $usrnm="root";
+// $pass="";
+// $dbname="formvalidation";
+// $con=mysqli_connect($server,$usrnm,$pass,$dbname);
 if(!$con){
     die("connection to this server failed due to ".mysqli_connect_errno());
 }
